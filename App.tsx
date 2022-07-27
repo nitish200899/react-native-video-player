32d6a04825deb4350dc24737aed47964e7eb350e
nitishkesarwani <nitishkesarwani20@gmail.com>
2022-07-27 11:39:53 +0530

    import comment

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React,{FC} from 'react'
 import {View,SafeAreaView,Text,StyleSheet} from 'react-native'
 import VideoPlayerComponent from './components/VideoPlayer'
-//abc
+//all the imports
 
 const App : FC = ()=>{
   return (
